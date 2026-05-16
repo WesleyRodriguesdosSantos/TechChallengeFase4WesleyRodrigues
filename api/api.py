516d916b27a9352d5de5ae3eae03eaeada802fdc
Wesley Rodrigues dos Santos <70420138+WesleyRodriguesdosSantos@users.noreply.github.com>
2026-05-15 23:48:29 -0300

    Use environment variable for model path

--- api/api.py
+++ api/api.py
@@ -14,7 +14,8 @@ from prometheus_fastapi_instrumentator import Instrumentator
 
 
 # Caminho para o modelo pré-treinado
-MODEL_PATH = "api/modelo_lstm_wege3.keras"
+import os
+MODEL_PATH = os.getenv("MODEL_PATH", "modelo_lstm_wege3.keras")
 model = load_model(MODEL_PATH)
 
 
