debbbe86ab7e7c03216ee990ff80b33284c11e07
Wesley Rodrigues dos Santos <70420138+WesleyRodriguesdosSantos@users.noreply.github.com>
2026-05-15 23:37:59 -0300

    Change MODEL_PATH to hardcoded value
    Update MODEL_PATH to use a hardcoded path instead of an environment variable.

--- api/api.py
+++ api/api.py
@@ -14,9 +14,9 @@ from prometheus_fastapi_instrumentator import Instrumentator
 
 
 # Caminho para o modelo pré-treinado
-import os
-MODEL_PATH = os.getenv("MODEL_PATH")
+MODEL_PATH = "api/modelo_lstm_wege3.keras"
 model = load_model(MODEL_PATH)
+
 
 
 scaler = MinMaxScaler(feature_range=(0, 1))
